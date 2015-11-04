2677e38c912b0f490e3392432ad6b8e029b4bc7e
Ewan Nisbet <ewan.nisbet@hotmail.com>
2015-11-04 17:17:42 +1100

    moved demo code in class_obj.py to a distinct function

--- class_obj.py
+++ class_obj.py
@@ -1,18 +1,3 @@
-my_prices = []
-my_prices.append({'id': 46534})
-my_prices.append({'id': 49445})
-my_prices.append({'id': 876})
-my_prices.append({'id': 49859})
-my_prices.append({'id': 53507})
-my_prices.append({'id': 20963})
-my_prices.append({'id': 272})
-my_prices.append({'id': 906})
-my_prices.append({'id': 21})
-my_prices.append({'id': 55})
-my_prices.append({'id': 57})
-my_prices.append({'id': 60})
-my_prices.append({'id': 420})
-my_prices.append({'id': 418})
 
 class prices_container:
     container_count = (0)
@@ -31,22 +16,6 @@ class prices_container:
     def returnSelf(self):
         return self.container
 
-
-price_container = prices_container(my_prices)
-'''
-price_container.displayContainer()
-price_container.deleteItem({'id': 906})
-print
-price_container.displayContainer()
-print
-price_container.addItem({'id': 906})
-price_container.displayContainer()
-'''
-
-my_hw = {}
-my_hw['hostname'] = 'test1'
-my_hw['domain'] = 'example.com'
-
 class hardware_container:
     container_count = (0)
     def __init__(self, hardware_dict):
@@ -64,32 +33,6 @@ class hardware_container:
     def returnSelf(self):
         return self.container
 
-
-hw_container = hardware_container(my_hw)
-'''
-hw_container.displayContainer()
-print
-hw_container.deleteItem('hostname')
-hw_container.displayContainer()
-my_oo = {}
-my_oo['hostname'] = 'test1'
-hw_container.addItem(my_oo)
-print
-hw_container.displayContainer()
-hw_container.displayCount()
-'''
-
-my_oc = {}
-my_oc['complexType'] = 'SoftLayer_Container_Product_Order_Hardware_Server'
-my_oc['quantity'] = 1
-my_oc['hardware'] = [hw_container.returnSelf()]
-#my_oc['containerIdentifier'] = 'myContainer'
-my_oc['packageId'] = '248'
-my_oc['prices'] = price_container.returnSelf()
-    #prices doesn't need [] because of datatype?
-my_oc['location'] = '449596'
-
-
 class product_container:
     container_count = (0)
     def __init__(self, order_dict):
@@ -105,25 +48,6 @@ class product_container:
         self.container = dict(self.container, **item)
     def returnSelf(self):
         return self.container
-
-
-product1 = product_container(my_oc)
-'''
-product1.displayContainer()
-print
-product1.deleteItem('complexType')
-product1.displayContainer()
-my_oo = {}
-my_oo['complexType'] = 'SoftLayer_Container_Product_Order_Hardware_Server'
-product1.addItem(my_oo)
-'''
-print
-#product1.displayContainer()
-#print product1.returnSelf()
-#product1.displayCount()
-
-my_order = {}
-my_order['orderContainers'] = [product1.returnSelf()]
 
 class order_container:
     container_count = (0)
@@ -141,23 +65,101 @@ class order_container:
     def returnSelf(self):
         return self.container
 
-order = order_container(my_order)
 
-import pprint
-from pprint import pprint as pp
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(order.returnSelf())
 
-import SoftLayer
-#increase default timeout to reduce false failure
-#on slow or conjested connections
-client = SoftLayer.create_client_from_env(timeout=240)
-client['Product_Order'].verifyOrder(order.returnSelf());
-client['Product_Order'].placeQuote(order.returnSelf());
+def list_objects_of_type(obj_type):
+    import gc
+    for obj in gc.get_objects():
+        if isinstance(obj, obj_type):
+            print obj.container
 
-'''
-import gc
-for obj in gc.get_objects():
-    if isinstance(obj, order_container):
-        print obj.containerID, obj.packageID
-'''
+def localDemo():
+    my_prices = []
+    my_prices.append({'id': 46534})
+    my_prices.append({'id': 49445})
+    my_prices.append({'id': 876})
+    my_prices.append({'id': 49859})
+    my_prices.append({'id': 53507})
+    my_prices.append({'id': 20963})
+    my_prices.append({'id': 272})
+    my_prices.append({'id': 906})
+    my_prices.append({'id': 21})
+    my_prices.append({'id': 55})
+    my_prices.append({'id': 57})
+    my_prices.append({'id': 60})
+    my_prices.append({'id': 420})
+    my_prices.append({'id': 418})
+
+    my_hw = {}
+    my_hw['hostname'] = 'test1'
+    my_hw['domain'] = 'example.com'
+
+    price_container = prices_container(my_prices)
+
+    price_container.displayContainer()
+    price_container.deleteItem({'id': 906})
+    print
+    price_container.displayContainer()
+    print
+    price_container.addItem({'id': 906})
+    price_container.displayContainer()
+
+
+    hw_container = hardware_container(my_hw)
+
+    hw_container.displayContainer()
+    print
+    hw_container.deleteItem('hostname')
+    hw_container.displayContainer()
+    my_oo = {}
+    my_oo['hostname'] = 'test1'
+    hw_container.addItem(my_oo)
+    print
+    hw_container.displayContainer()
+    hw_container.displayCount()
+
+
+    my_oc = {}
+    my_oc['complexType'] = 'SoftLayer_Container_Product_Order_Hardware_Server'
+    my_oc['quantity'] = 1
+    my_oc['hardware'] = [hw_container.returnSelf()]
+    #my_oc['containerIdentifier'] = 'myContainer'
+    my_oc['packageId'] = '248'
+    my_oc['prices'] = price_container.returnSelf()
+        #prices doesn't need [] because of datatype?
+    my_oc['location'] = '449596'
+
+    product1 = product_container(my_oc)
+
+    product1.displayContainer()
+    print
+    product1.deleteItem('complexType')
+    product1.displayContainer()
+    my_oo = {}
+    my_oo['complexType'] = 'SoftLayer_Container_Product_Order_Hardware_Server'
+    product1.addItem(my_oo)
+
+    print
+    #product1.displayContainer()
+    #print product1.returnSelf()
+    #product1.displayCount()
+
+    my_order = {}
+    my_order['orderContainers'] = [product1.returnSelf()]
+
+
+    order = order_container(my_order)
+
+    import pprint
+    from pprint import pprint as pp
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(order.returnSelf())
+
+    import SoftLayer
+    #increase default timeout to reduce false failure
+    #on slow or conjested connections
+    #client = SoftLayer.create_client_from_env(timeout=240)
+    #client['Product_Order'].verifyOrder(order.returnSelf());
+    #client['Product_Order'].placeQuote(order.returnSelf());
+
+    list_objects_of_type(order_container)
